b22947e669f3923876b36a392996fcca21b1f233
xtgby <2385074551@qq.com>
2022-09-28 15:51:03 +0800

    git_bashrc.sh add

---
+++ git_bashrc.sh
@@ -0,0 +1,4 @@
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWUNTRACKEDFILES=true
+source /usr/share/git-core/contrib/completion/git-prompt.sh
+export PS1='\[\033[01;35m\][\[\033[01;32m\]\h\[\033[01;34m\] \W\[\033[31m\]\[\033[01;35m\]] \[\033[01;34m\]$\[\033[00m\]'
